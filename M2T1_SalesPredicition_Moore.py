470ea1810198cab72f329ffc2060280c6188a687
moorea8239 <31700569+moorea8239@users.noreply.github.com>
2017-09-06 11:06:56 -0400

    Add files via upload

---
+++ M2T1_SalesPredicition_Moore.py
@@ -0,0 +1,20 @@
+# CTI-110
+# M2T1 - Sales Prediction
+# Andrew Moore
+# 8/30
+
+
+# Suggested variables: totalSales, annualProfit
+
+# ask the user for total sales
+totalSales = int (input("Type Total Sales: "))
+
+# calulate the profit amount
+annualProfit = 0.23 * totalSales
+
+# Print the profit amount
+print (annualProfit)
+
+
+
+#userTyped = input("Type a number: ")
