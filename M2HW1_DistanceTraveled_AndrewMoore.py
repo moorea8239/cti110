5b4eb2e981b80800974840d767abd51527488036
moorea8239 <31700569+moorea8239@users.noreply.github.com>
2017-12-10 21:54:31 -0500

    Add files via upload

---
+++ M2HW1_DistanceTraveled_AndrewMoore.py
@@ -0,0 +1,17 @@
+# CTI-110
+# Andrew Moore
+# M2T1 - Distance Traveled
+# 09/06
+
+# Calculate distance traveled
+
+# Variables distance
+
+distance = 70 * 6
+print ("The distance the car will travel in 6 hours is," , distance )
+
+distance = 70 * 10
+print ("The distance the car will travel in 10 hours is," , distance )
+
+distance = 70 * 15
+print ("The distance the car will travel in 15 hours is," , distance )
